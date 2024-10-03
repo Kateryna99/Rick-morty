1ad2454ebcf4ff3d1811eec5836454c117dee426
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-03 21:48:55 +0200

    test2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: 'Rick-Morty',
+    basePath: '/Rick-Morty"',
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
         <h1>Rick and Morty</h1>
+        <p>Welcome to the Rick and Morty website!</p>
     </div>
   );
 }
