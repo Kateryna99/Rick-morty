dff97b3944e9ffc7cc8a809a76178b7fff582aa5
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-14 18:22:03 +0200

    fix misspell

--- src/types/Character.ts
+++ src/types/Character.ts
@@ -1,16 +1,16 @@
-import { Location } from './Location';
+import { Location } from "./Location";
 
 export interface Character {
-    id: number;
-    name: string;
-    status: string;
-    species: string;
-    type: string;
-    gender: string;
-    origin: Partial<Location>;
-    location: Partial<Location>;
-    image: string;
-    episodes: string[];
-    url: string;
-    created: string;
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: Partial<Location>;
+  location: Partial<Location>;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
 }
