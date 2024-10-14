fc099a10dbcbd9f5388c2d413e46ef4a1d8e71d8
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-14 18:25:15 +0200

    eslint test 10

--- src/components/BaseCatalogPage/BaseCatalogContent.tsx
+++ src/components/BaseCatalogPage/BaseCatalogContent.tsx
@@ -1,13 +1,23 @@
-import styles from "./BaseCatalogContent.module.scss"
+import styles from "./BaseCatalogContent.module.scss";
 import classNames from "classnames";
+import { FC, ReactNode } from "react";
 
-
-export const BaseCatalogContent = ({children, flex}) => {
-    return (
-        <div className={styles.catalog}>
-            <div className={classNames(styles.catalogWrapper, { [styles.flex]: flex })}>
-                {children}
-            </div>
-        </div>
-    )
+interface BaseCatalogContentProps {
+  children: ReactNode;
+  flex?: boolean;
 }
+
+export const BaseCatalogContent: FC<BaseCatalogContentProps> = ({
+  children,
+  flex,
+}) => {
+  return (
+    <div className={styles.catalog}>
+      <div
+        className={classNames(styles.catalogWrapper, { [styles.flex]: flex })}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
