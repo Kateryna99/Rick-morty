0d62cf6c267177bccf7cd718bd24585257249d43
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-22 21:50:46 +0200

    session storage fix

--- src/hooks/useHandleFirstSession.ts
+++ src/hooks/useHandleFirstSession.ts
@@ -3,30 +3,14 @@
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
-function getBoolean(key: string): boolean | null {
-  const value = sessionStorage.getItem(key);
-
-  if (!value) {
-    return null;
-  }
-
-  return JSON.parse(value) as boolean;
-}
-
-function setBoolean(key: string, value: boolean): void {
-  sessionStorage.setItem(key, JSON.stringify(value));
-}
-
 export const useHandleFirstSession = () => {
-  const [isLoading, setIsLoading] = useState(!getBoolean("wasVisited"));
+  const [isLoading, setIsLoading] = useState(true);
   const path = usePathname();
 
   useEffect(() => {
     if (isLoading) {
       setTimeout(() => setIsLoading(false), 5000);
     }
-
-    setBoolean("wasVisited", true);
   }, [isLoading]);
 
   return { isLoading, path };
